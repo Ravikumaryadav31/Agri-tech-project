f5beb626d07694e6436c2cc7a2de5bc1781404ea
ravi <ravikumar2hk>
2024-12-11 15:28:51 +0530

    changesss

--- src/components/fisrtPage.jsx
+++ src/components/fisrtPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Navbar from "./navibar"; 
 import Carosel from './caro';
 import './shoplist.css';
-import Footer from './footer'; 
+import Footer from './foot'; 
 
 const FisrtPage = () => {
   const navigate = useNavigate();
